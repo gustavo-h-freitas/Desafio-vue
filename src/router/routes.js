ec7e0afeb0bd229ef3712cecb67caaa5fea47156
lucenalbd <lucena.lbd@gmail.com>
2021-07-15 06:51:19 -0300

    Modificações nas rotas de navegação.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,18 @@
 
 const routes = [
   {
-    path: '/',
+    path: '/dashboard',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: 'produtos', component: () => import('pages/Produtos.vue') },
       { path: 'vendedores', component: () => import('pages/Vendedores.vue') },
-      { path: 'relatorios', component: () => import('pages/Relatorios.vue') }
+      { path: 'relatorios', component: () => import('pages/Relatorios.vue') },
+      { path: 'loja', component: () => import('pages/Loja.vue') }
     ]
   },
   {
-    path: '/login',
+    path: '/',
     component: () => import('layouts/auth/Auth.vue'),
     children: [
       { path: '', component: () => import('pages/Auth.vue') }
