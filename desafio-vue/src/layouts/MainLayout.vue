c247006ab95f3812926046339b124dd91b04d38c
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-08-01 21:37:55 -0300

    Estilizando toolbar

--- desafio-vue/src/layouts/MainLayout.vue
+++ desafio-vue/src/layouts/MainLayout.vue
@@ -1,21 +1,31 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+      <q-toolbar style="width: 100%">
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <div class="row">
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="toggleLeftDrawer"
+          />
 
-        <div>Quasar v{{ $q.version }}</div>
+          <div class="row items-center">SIG VENDAS v0.1</div>
+
+        </div>
+
+        <div class="row q-ma-lg">
+          <div class="justify-end">
+            <img
+              src="../assets/leadbase.png"
+              alt="logo leadbase"
+              style="height: 30px;"
+            >
+          </div>
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -23,14 +33,14 @@
       v-model="leftDrawerOpen"
       show-if-above
       bordered
-      class="bg-grey-1"
+      class="bg-white-1"
     >
       <q-list>
         <q-item-label
           header
-          class="text-grey-8"
+          class="text-white-8"
         >
-          Essential Links
+          SISTEMA ADMINISTRATIVO
         </q-item-label>
 
         <EssentialLink
@@ -52,46 +62,40 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: 'Dashboard',
+    caption: 'Tela de início',
+    icon: 'home',
+    link: '/dashboard'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: 'Produtos',
+    caption: 'Cadastro, edição e exclusão',
+    icon: 'add_box',
+    link: '/dashboard/produtos'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: 'Vendedores',
+    caption: 'Cadastro, edição e exclusão',
+    icon: 'group',
+    link: '/dashboard/vendedores'
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: 'Relatórios',
+    caption: '',
+    icon: 'pie_chart',
+    link: '/dashboard/relatorios'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: 'Loja',
+    caption: 'Local para vendas',
+    icon: 'store_mall_directory',
+    link: '/dashboard/loja'
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Logout',
+    caption: 'No momento redireciona para a tela que seria de Login*',
+    icon: 'arrow_back',
+    link: '/'
   }
 ]
 
@@ -117,3 +121,5 @@ export default defineComponent({
   }
 })
 </script>
+
+
