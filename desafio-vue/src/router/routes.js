9a3cd489911f29321fde633b880bdc634d076b6c
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-08-01 21:39:19 -0300

    Criando as rotas e importando os componentes

--- desafio-vue/src/router/routes.js
+++ desafio-vue/src/router/routes.js
@@ -4,7 +4,18 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'produtos', component: () => import('pages/Produtos.vue') },
+      { path: 'vendedores', component: () => import('pages/Vendedores.vue') },
+      { path: 'relatorios', component: () => import('pages/Relatorios.vue') },
+      { path: 'loja', component: () => import('pages/Loja.vue') }
+    ]
+  },
+  {
+    path: '/',
+    component: () => import('layouts/auth/Auth.vue'),
+    children: [
+      { path: '', component: () => import('pages/Auth.vue') }
     ]
   },
 
