757a3e74ccf532adfab4de57e831dd9e3857a225
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-08-02 07:57:32 -0300

    Ajustando arquivo firebase

--- desafio-vue/src/boot/firebase.js
+++ desafio-vue/src/boot/firebase.js
@@ -1,14 +1,16 @@
-import firebase from 'firebase'
+/* eslint-disable */
+import firebase from "firebase";
 
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyCCbC13W0H-mOWMAAz0d2XgL7D5KT6b1HU",
   authDomain: "desafio-vue-bb88a.firebaseapp.com",
   projectId: "desafio-vue-bb88a",
   storageBucket: "desafio-vue-bb88a.appspot.com",
   messagingSenderId: "845929965050",
   appId: "1:845929965050:web:5950718ff1b7347d17b043"
-}
+};
 
 let firebaseApp = firebase.initializeApp(firebaseConfig);
+let db = firebaseApp.firestore();
 
-export { firebase }
+export { db, firebase };
